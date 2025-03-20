2366fd273e5113539fec02e1fab10b6f8c880855
Kuba Vejražka <jakvejr@gmail.com>
2025-03-20 12:05:56 +0100

    minor ui updates, db scheme update

--- src/lib/components/searchbar.svelte
+++ src/lib/components/searchbar.svelte
@@ -7,14 +7,14 @@
 	<div class="mb-4 flex w-full p-4">
 		<input
 			type="text"
-			class="grow rounded-l-full bg-background-50 px-4 outline-none transition-colors placeholder:text-background-200"
+			class="grow rounded-l-full bg-background-100 px-4 outline-none transition-colors placeholder:text-background-200"
 			placeholder="Search your notes"
 			bind:value={search}
 		/>
 		<input type="submit" class="hidden" />
 		<button
 			onclick={() => console.log(search)}
-			class="flex items-center justify-center rounded-r-full bg-background-50 p-2 text-background-200 transition-colors"
+			class="flex items-center justify-center rounded-r-full bg-background-100 p-2 text-background-200 transition-colors"
 		>
 			<span class="material-symbols-outlined">search</span>
 		</button>
